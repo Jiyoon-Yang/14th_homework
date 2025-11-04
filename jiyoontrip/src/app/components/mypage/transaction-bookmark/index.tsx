07c59d9c24987503666aaa0ee9acd38ec5d823b2
Jiyoon-Yang <jiyoon3522@naver.com>
2025-11-04 16:35:35 +0900

    refactor: 마이페이지 공통 레이아웃 분리 및 /mypage 경로에 적용

--- jiyoontrip/src/app/components/mypage/transaction-bookmark/index.tsx
+++ jiyoontrip/src/app/components/mypage/transaction-bookmark/index.tsx
@@ -11,60 +11,7 @@ export default function TransactionBookmark() {
   const [activeTab, setActiveTab] = useState<"myProduct" | "bookmark">("myProduct");
 
   return (
-    <div className={styles.container}>
-      <div className={styles.gap}></div>
-
-      <div className={styles.mypageLabel}>
-        <h1 className={styles.title}>마이 페이지</h1>
-      </div>
-      <div className={styles.gap}></div>
-      <div className={styles.myprofile}>
-        <div className={styles.userInfoBox}>
-          <div className={styles.profileSection}>
-            <div className={styles.profileImage}></div>
-            <span className={styles.profileName}>김상훈</span>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.pointSection}>
-            <Image src="/icons/outline/point.svg" alt="포인트" width={24} height={24} />
-            <div className={styles.pointValue}>
-              <span className={styles.pointNumber}>23,000</span>
-              <span className={styles.pointUnit}>P</span>
-            </div>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.menuSection}>
-            <button className={`${styles.menuTab} ${styles.menuTabActive}`}>
-              <span className={styles.menuTabText}>거래내역&북마크</span>
-              <Image
-                src="/icons/outline/right_arrow.svg"
-                alt="이동"
-                width={20}
-                height={20}
-              />
-            </button>
-            <button className={styles.menuTab}>
-              <span className={styles.menuTabText}>포인트 사용 내역</span>
-              <Image
-                src="/icons/outline/right_arrow.svg"
-                alt="이동"
-                width={20}
-                height={20}
-              />
-            </button>
-            <button className={styles.menuTab}>
-              <span className={styles.menuTabText}>비밀번호 변경</span>
-              <Image
-                src="/icons/outline/right_arrow.svg"
-                alt="이동"
-                width={20}
-                height={20}
-              />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.gap}></div>
+    <>
       <div className={styles.tabs}>
         <button
           className={`${styles.tabButton} ${
@@ -175,6 +122,6 @@ export default function TransactionBookmark() {
         </div>
       </div>
       <div className={styles.gap}></div>
-    </div>
+    </>
   );
 }
